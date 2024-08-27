f3b961729cd7bc1204eb09a3ea7b8197909249ed
Vexionix <djdarkstefan@gmail.com>
2024-08-27 15:50:13 +0300

    add Core project and models for db tables

---
+++ Forum/Forum.Core/Entities/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forum.Core.Entities
+{
+	internal class Comment
+	{
+		public int Id { get; set; }
+		public string Content { get; set; }
+		public int Likes { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public bool IsEdited { get; set; }
+		public DateTime LastEditedAt { get; set; }
+		public bool IsDeleted { get; set; }
+		public bool IsRemoved { get; set; }
+		public User User { get; set; }
+		public Post Post { get; set; }
+
+	}
+}
---
+++ Forum/Forum.Core/Entities/Title.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forum.Core.Entities
+{
+	internal class Title
+	{
+		public int Id { get; set; }
+		public string TitleName { get; set; }
+	}
+}
