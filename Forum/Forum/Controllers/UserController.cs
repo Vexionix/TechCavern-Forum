af5627feace9878576778c8c9fd4cbc4e661a410
Mircea-Stefan Perju <perjumirceastefan@gmail.com>
2024-08-27 14:42:40 +0000

    Api and testing

--- Forum/Forum/Controllers/UserController.cs
+++ Forum/Forum/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Forum.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Forum.Controllers
@@ -7,5 +8,21 @@ namespace Forum.Controllers
 	[ApiController]
 	public class UserController : ControllerBase
 	{
+		[HttpPost]
+		public ActionResult<User> CreateUser([FromBody] UserCreate userCreateBody)
+		{
+			var newUser = new User()
+			{
+				Id = 1,
+				Username = userCreateBody.Username,
+				Email = userCreateBody.Email,
+				Password = userCreateBody.Password,
+				Description = "We don't know much about them, but we are sure they are cool.",
+				Title = "Member",
+				Role = "Member",
+				CreatedAt = DateTime.Now
+			};
+			return StatusCode(StatusCodes.Status201Created, newUser);
+		}
 	}
 }
