cf30901d2f88ab61ab64e001bc08202548137e3c
Perju Mircea-Stefan <perjumirceastefan@gmail.com>
2024-08-28 18:24:05 +0300

    add data project

---
+++ Forum/Forum.Data/DbContexts/ForumDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Forum.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Data.DbContexts
+{
+	public class ForumDbContext : DbContext
+	{
+		public DbSet<Category> Categories {  get; set; }
+		public DbSet<Comment> Comment { get; set; }
+		public DbSet<Post> Post { get; set; }
+		public DbSet<Subcategory> Subcategories { get; set; }
+		public DbSet<Title> Titles { get; set; }
+		public DbSet<User> Users { get; set; }
+
+		public ForumDbContext(DbContextOptions options): base(options) { }
+	}
+}
