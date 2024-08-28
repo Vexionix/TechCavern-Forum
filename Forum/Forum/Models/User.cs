502f706a7a95bf2b31a47f0aa886893d20712927
Mircea-Stefan Perju <perjumirceastefan@gmail.com>
2024-08-28 15:30:32 +0000

    update models and controllers

--- Forum/Forum/Models/User.cs
+++ Forum/Forum/Models/User.cs
@@ -6,7 +6,7 @@
 		public string Username { get; set; }
 		public string Email { get; set; }
 		public string Password { get; set; }
-		public string Description { get; set; }
+		public string Bio { get; set; }
 		public string Title { get; set; }
 		public string Role { get; set; }
 		public DateTime CreatedAt { get; set; }
