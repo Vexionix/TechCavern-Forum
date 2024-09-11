8467bbdd44c1ce894aea7c307c7025e30dd6dd03
Perju Mircea-Stefan <perjumirceastefan@gmail.com>
2024-09-11 11:47:14 +0300

    move auth related routes to authcontroller and add jwt generation

--- Forum/Forum/Controllers/UsersController.cs
+++ Forum/Forum/Controllers/UsersController.cs
@@ -11,12 +11,10 @@ namespace Forum.Controllers
 	public class UsersController : ControllerBase
 	{
 		private readonly IUserRepository _userRepository;
-		private readonly IPasswordService _passwordService;
 
 		public UsersController(IUserRepository userRepository, IPasswordService passwordService)
 		{
 			_userRepository = userRepository;
-			_passwordService = passwordService;
 		}
 
 		[HttpGet]
@@ -39,49 +37,6 @@ namespace Forum.Controllers
 			}).ToList();
 
 			return Ok(userDtos);
-		}
-
-		[HttpPost("register")]
-		public async Task<ActionResult> RegisterUser([FromBody] UserRegister userRegisterBody)
-		{
-			if (await _userRepository.UserAlreadyExists(userRegisterBody.Username, userRegisterBody.Email) == true)
-			{
-				return StatusCode(StatusCodes.Status400BadRequest, "User with the same username or email already exists");
-			}
-
-			if (!_passwordService.CheckCriteria(userRegisterBody.Password))
-			{
-				return StatusCode(StatusCodes.Status400BadRequest, "Password does not meet criteria.");
-			}
-
-			var newUser = new User(userRegisterBody.Username,
-				userRegisterBody.Email,
-				_passwordService.Encrypt(userRegisterBody.Password), 
-				"Member",
-				"We don't know much about them, but we are sure they are cool.",
-				"Romania");
-
-			await _userRepository.AddUser(newUser);
-
-			return StatusCode(StatusCodes.Status201Created);
-		}
-
-		[HttpPost("login")]
-		public async Task<ActionResult> LoginUser([FromBody] UserLogin userLoginBody)
-		{
-			var user = await _userRepository.GetUserByUsername(userLoginBody.Username);
-
-			if (user is null)
-			{
-				return StatusCode(StatusCodes.Status400BadRequest, "No user with such username exists.");
-			}
-
-			if (!_passwordService.CheckMatchingHash(userLoginBody.Password, user.PasswordHash))
-			{
-				return StatusCode(StatusCodes.Status400BadRequest, "Wrong password.");
-			}
-			
-			return StatusCode(StatusCodes.Status200OK);
 		}
 	}
 }
